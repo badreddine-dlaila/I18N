669f8aa90db6c92c499cae8a6a0a8c3f5b770b58
alexinea <forerunner@outlook.com>
2019-07-12 15:10:46 +0800

    Update comments for Text

--- src/Cosmos.I18N/Cosmos/I18N/Text.cs
+++ src/Cosmos.I18N/Cosmos/I18N/Text.cs
@@ -3,37 +3,77 @@ using Cosmos.I18N.Core;
 using Cosmos.I18N.Core.Extensions;
 using Cosmos.I18N.Languages;
 
-namespace Cosmos.I18N {
-    public struct Text : IEquatable<Text> {
+namespace Cosmos.I18N
+{
+    /// <summary>
+    /// Text
+    /// </summary>
+    public struct Text : IEquatable<Text>
+    {
         private Locale Language { get; set; }
         private string ResourceKey { get; set; }
         private string OriginText { get; set; }
         private object[] FormatingParameters { get; set; }
 
+        /// <summary>
+        /// Create a new instance of <see cref="Text"/>.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="resourceKey"></param>
+        /// <param name="language"></param>
         public Text(string text, string resourceKey, Locale language) : this(text, resourceKey, language, null) { }
 
-        public Text(string text, string resourceKey, Locale language, params object[] parameters) {
+        /// <summary>
+        /// Create a new instance of <see cref="Text"/>.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="resourceKey"></param>
+        /// <param name="language"></param>
+        /// <param name="parameters"></param>
+        public Text(string text, string resourceKey, Locale language, params object[] parameters)
+        {
             Language = language;
             ResourceKey = resourceKey;
             OriginText = text;
             FormatingParameters = parameters;
         }
 
+        /// <summary>
+        /// Convert to String
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         public static implicit operator string(Text t) => t.ToString();
 
-        public override string ToString() {
+        /// <summary>
+        /// To String.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
             var processor = StaticInstanceForILanguageServiceProvider.Instance.GetTranslationProcessor();
             var text = processor.Translate(Language, ResourceKey, OriginText);
-            if (FormatingParameters != null) {
+            if (FormatingParameters != null)
+            {
                 text = string.Format(text, FormatingParameters);
             }
-            
+
             return text;
         }
 
+        /// <summary>
+        /// Text hashcode
+        /// </summary>
+        /// <returns></returns>
         public int TextHashCode() => OriginText.GetHashCode();
 
-        public bool Equals(Text other) {
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Text other)
+        {
             return Language == other.Language &&
                    ResourceKey == other.ResourceKey &&
                    OriginText == other.OriginText &&
