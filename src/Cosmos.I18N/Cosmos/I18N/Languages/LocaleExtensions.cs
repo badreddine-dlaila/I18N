150b62d549e361ccac951f91b8e73484f6094a18
alexinea <forerunner@outlook.com>
2019-09-17 15:17:18 +0800

    Add: convert Locale to LanguageTag.

--- src/Cosmos.I18N/Cosmos/I18N/Languages/LocaleExtensions.cs
+++ src/Cosmos.I18N/Cosmos/I18N/Languages/LocaleExtensions.cs
@@ -1,19 +1,52 @@
-﻿using System;
+﻿using Cosmos.I18N.Core.Extensions;
 using EnumsNET;
 
 namespace Cosmos.I18N.Languages
 {
+    /// <summary>
+    /// Locale extensions
+    /// </summary>
     public static class LocaleExtensions
     {
-        public static string ToLocaleString(this Locale locale)
+        /// <summary>
+        /// Convert <see cref="Locale"/> to <see cref="LanguageTag"/>.
+        /// </summary>
+        /// <param name="locale"></param>
+        /// <returns></returns>
+        public static LanguageTag AsLanguageTag(this Locale locale)
         {
-            return locale.GetName();
+            return LanguageTagManager.Create(locale.GetLanguageTagText());
         }
 
-        public static Locale ToLocale(this string lang)
+        /// <summary>
+        /// Convert <see cref="LanguageTag"/> to <see cref="Locale"/>.
+        /// </summary>
+        /// <param name="languageTag"></param>
+        /// <returns></returns>
+        public static Locale AsLocale(this LanguageTag languageTag)
         {
-            if (string.IsNullOrWhiteSpace(lang)) throw new ArgumentNullException(nameof(lang));
-            return EnumsNET.Enums.GetMember<Locale>(lang, true).Value;
+            if (languageTag == null)
+                languageTag = LanguageTag.Current;
+
+            if (languageTag.Region.IsSet())
+            {
+                if (EnumsNET.Enums.TryParse(
+                    $"{languageTag.Language}_{languageTag.Region.ToUpperInvariant()}",
+                    true,
+                    out Locale locale1))
+                {
+                    return locale1;
+                }
+            }
+
+            return EnumsNET.Enums.TryParse(languageTag.Language, true, out Locale locale2)
+                ? locale2
+                : LanguageTag.DefaultLocale;
+        }
+
+        internal static string GetLanguageTagText(this Locale locale)
+        {
+            return locale.GetName().Replace("_", "-");
         }
     }
 }
